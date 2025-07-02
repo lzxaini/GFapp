78d2ef3c41f8982fed3b98c76b6a7dc6844cd3f0
17630921248 <1245634367@qq.com>
2025-07-02 15:20:23 +0800

    change: 修改二维码LOGO

--- pages/work-team/work-team.js
+++ pages/work-team/work-team.js
@@ -21,7 +21,7 @@ Page({
       text: '123',
       // v1.0.0+版本支持在二维码上绘制图片
       image: {
-        imageResource: '../../static/icon/team_1.png', // 不支持网络图片，如果非得网络图片，需要使用wx.getImageInfo 去获取图片信息，我这边往中间加的一个白图然后采用覆盖的方式
+        imageResource: '../../static/icon/gf_logo_w.png', // 不支持网络图片，如果非得网络图片，需要使用wx.getImageInfo 去获取图片信息，我这边往中间加的一个白图然后采用覆盖的方式
         dx: 100,
         dy: 100,
         dWidth: 37,
