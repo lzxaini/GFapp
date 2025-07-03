d186602f0079f8139d6f3b10d426dcf35b6d4f3d
17630921248 <1245634367@qq.com>
2025-07-03 11:50:09 +0800

    feature: 个人二维码生成和保存本地

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,13 @@ import {
   addVisitorApi
 } from '../../api/api.js'
 import { onMqttReady } from '../../utils/mqttReady';
+import drawQrcode from '../../utils/weapp.qrcode.min'
+import tool from '../../utils/tools'
 Page({
   data: {
     capsuleHeight: app.globalData.capsuleHeight,
+    qrCodeBox: false,
+    qrFlag: false,
   },
   onLoad() {
     onMqttReady(() => {
@@ -56,6 +60,69 @@ Page({
         });
       }
     });
+  },
+
+  drawUserQrcode() {
+    let _this = this;
+    drawQrcode({
+      width: 240,
+      height: 240,
+      canvasId: 'myQrcode',
+      text: '123',
+      // v1.0.0+版本支持在二维码上绘制图片
+      image: {
+        imageResource: '../../static/icon/gf_logo_w.png', // 不支持网络图片，如果非得网络图片，需要使用wx.getImageInfo 去获取图片信息，我这边往中间加的一个白图然后采用覆盖的方式
+        dx: 100,
+        dy: 100,
+        dWidth: 50,
+        dHeight: 50
+      }
+    })
+    setTimeout(() => {
+      _this.setData({
+        qrFlag: true
+      })
+    }, 500);
+  },
+  // 邀请加入
+  openQrCode() {
+    this.setData({
+      qrCodeBox: true
+    })
+    if (this.data.qrCodeBox) {
+      this.drawUserQrcode()
+    }
+  },
+  // 保存邀请码
+  saveQrCode: tool.debounce(function () {
+    let _this = this;
+    wx.canvasToTempFilePath({
+      canvasId: 'myQrcode',
+      success(res) {
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success() {
+            wx.showToast({
+              title: '保存成功',
+              icon: 'success'
+            })
+            _this.closeDialog()
+          },
+          fail(err) {
+            console.error('保存失败', err)
+          }
+        })
+      },
+      fail(err) {
+        console.error('生成二维码失败', err)
+      }
+    })
+  }, 800),
+  closeDialog() {
+    this.setData({
+      qrCodeBox: false,
+      qrFlag: false
+    })
   },
   goWorkTeam() {
     wx.navigateTo({
