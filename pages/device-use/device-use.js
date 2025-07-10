527d4479f722d8d4d47063fcb620994e64155746
17630921248 <1245634367@qq.com>
2025-07-10 10:40:38 +0800

    feature: 设备使用页面增加deviceId显示

--- pages/device-use/device-use.js
+++ pages/device-use/device-use.js
@@ -1,1 +1,10 @@
-Page({})
+const app = getApp()
+Page({
+  data: {
+    marginBottom: app.globalData.marginBottom,
+    deviceId: ''
+  },
+  onLoad(option) {
+    console.log("🥵 ~ option: ", option)
+  }
+})
