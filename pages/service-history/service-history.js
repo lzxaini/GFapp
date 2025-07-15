dbfc963d86a0314d8158946c042a9d02f239d347
17630921248 <1245634367@qq.com>
2025-07-15 17:40:49 +0800

    feature: 页面滚动问题修复和新增触底加载

--- pages/service-history/service-history.js
+++ pages/service-history/service-history.js
@@ -10,6 +10,27 @@ Page({
     ],
     activeValue: '0',
   },
+
+  /**
+ * 页面上拉触底事件的处理函数
+ */
+  onReachBottom() {
+    console.log('触底',)
+    let { tableData, total } = this.data
+    if (tableData.length <= total) {
+      let pageNum = ++this.data.pageObj.pageNum
+      this.setData({
+        'pageObj.pageNum': pageNum
+      })
+      this.getRechargeRecords('bottom')
+    }
+  },
+  pullDownToRefresh() {
+    this.setData({
+      'pageObj.pageNum': 1
+    })
+    this.getRechargeRecords()
+  },
   tabClick(e) {
     const value = e.detail.value;
     this.setData({
