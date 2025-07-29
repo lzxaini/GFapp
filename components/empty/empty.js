becb5f98b30e63652ec82b10e3d27d36015cb83e
17630921248 <1245634367@qq.com>
2025-07-29 10:43:56 +0800

    feature: 新增空展示组件

---
+++ components/empty/empty.js
@@ -0,0 +1,14 @@
+Component({
+  properties: {
+    image: {
+      type: String,
+      value: '../../static/icon/empty2.png'
+    },
+    description: {
+      type: String,
+      value: '暂无相关内容'
+    }
+  },
+  data: {},
+  methods: {}
+})
