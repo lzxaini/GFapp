b289dfd83cdd0fe3655b8991294e3f9c9727d599
17630921248 <1245634367@qq.com>
2025-07-31 19:34:47 +0800

    feature: 新增结束服务设备指定时长无响应自动返回首页

--- pages/device-use/device-use.js
+++ pages/device-use/device-use.js
@@ -4,7 +4,8 @@ Page({
   data: {
     marginBottom: app.globalData.marginBottom,
     deviceFlag: false,
-    deviceId: ''
+    deviceId: '',
+    duration: 5000, // 3秒无响应则自动返回首页
   },
   onLoad(option) {
     let { deviceId } = option
@@ -20,6 +21,11 @@ Page({
     console.log('设备列表收到消息：', topic, message);
     let { msg, result } = message
     if (topic === `/resp/${this.data.deviceId}` && (result.funcCode === 5 && result.state === 2 || msg === '100000FF')) {
+      if (this.exitTimer) {
+        clearTimeout(this.exitTimer);
+        this.exitTimer = null;
+      }
+      wx.hideLoading();
       Message.success({
         context: this,
         offset: [90, 32],
@@ -34,7 +40,29 @@ Page({
     }
   },
   exitDevice() {
+    wx.showLoading({
+      title: '正在结束服务...',
+      mask: true,
+    });
     const mqttQrotocol = app.globalData.mqttQrotocol;
     mqttQrotocol.controlDevice(`/req/${this.data.deviceId}`, false, 255);
+    // 3秒无响应则自动返回首页
+    if (this.exitTimer) {
+      clearTimeout(this.exitTimer);
+    }
+    this.exitTimer = setTimeout(() => {
+      wx.hideLoading();
+      Message.error({
+        context: this,
+        offset: [90, 32],
+        duration: 2000,
+        content: '设备无响应，将自动返回首页！',
+      });
+      setTimeout(() => {
+        wx.reLaunch({
+          url: '/pages/index/index',
+        })
+      }, 1500);
+    }, this.data.duration);
   }
 })
