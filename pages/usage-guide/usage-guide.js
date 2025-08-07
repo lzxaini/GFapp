e10ebd7c9cb1132deaff8aa2cc559ef2e140327e
lzxaini <1245634367@qq.com>
2025-08-07 21:49:33 +0800

    feature: 新增使用指南

---
+++ pages/usage-guide/usage-guide.js
@@ -0,0 +1,23 @@
+import { showMessage } from '../../utils/tools';
+
+const app = getApp()
+Page({
+  data: {
+    imageUrl: 'https://cdn.fxnws.com/gfkm/Usage-Guide.jpg'
+  },
+  onLoad() {
+    wx.showLoading({
+      title: '正在加载...',
+    })
+  },
+  loadImage(){
+    wx.hideLoading()
+  },
+  errorImage(){
+    wx.hideLoading()
+    this.message('error', '使用指南加载失败，请稍后再试！', 3000)
+  },
+  message(type, text, duration = 1500) {
+    showMessage(type, text, duration, this);
+  }
+})
