0c8a70fb451e25b5b4caa5bea2d0c4fd19a6226d
17630921248 <1245634367@qq.com>
2025-08-12 17:37:54 +0800

    feature: 新增扫码加入部门逻辑

--- pages/team-search/team-search.js
+++ pages/team-search/team-search.js
@@ -1,3 +1,6 @@
+import Message from 'tdesign-miniprogram/message/index';
+import { joinDeptApi } from '../../api/api'
+const app = getApp()
 Page({
   data: {
     confirmBtn: { content: '确定', variant: 'base' },
@@ -15,7 +18,13 @@ Page({
       success: (res) => {
         const { result } = res;
         if (result) {
-          _this.joinDept(result)
+          const match = result.match(/^dept-(.+)$/);
+          if (match) {
+            const deptId = match[1]; // deptId
+            _this.joinDept(deptId)
+          } else { // 加入团队
+            _this.joinTeam(result)
+          }
         }
       },
       fail: (err) => {
@@ -24,9 +33,29 @@ Page({
     });
   },
   // 加入团队
-  joinDept(searchValue) {
+  joinTeam(searchValue) {
     wx.navigateTo({
       url: `/pages/team-join/team-join?searchValue=${searchValue}`,
     });
+  },
+  // 加入部门
+  joinDept(searchValue) {
+    const userInfo = app.globalData.userInfo
+    joinDeptApi(userInfo.userId, searchValue).then(res => {
+      if (res.code === 200) {
+        Message.success({
+          context: this,
+          offset: [90, 32],
+          duration: 1300,
+          content: '加入成功，即将跳转首页！',
+        });
+        // 扫码成功
+        setTimeout(() => {
+          wx.reLaunch({
+            url: '/pages/index/index'
+          });
+        }, 1500)
+      }
+    })
   }
 })
