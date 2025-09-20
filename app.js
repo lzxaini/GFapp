d5eb979170b77028d579b847d8444a11ead1ff58
17630921248 <1245634367@qq.com>
2025-09-20 15:28:23 +0800

    feature: 新版本

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ App({
     appName: {
       name: '光馥科美',
       slogan: '光塑年轻力，馥养无龄美',
-      appVersion: '0.0.14_25092012',
+      appVersion: '0.0.14_25092015',
     },
     phoneEnv: '', // 手机系统
     mqttClient: null,
