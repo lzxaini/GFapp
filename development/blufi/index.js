54467b68eefe99724815614dfb3c4e582d070127
17630921248 <1245634367@qq.com>
2025-09-22 12:31:36 +0800

    feature: 配网页面调整

--- development/blufi/index.js
+++ development/blufi/index.js
@@ -1,3 +1,10 @@
+import {
+  showMessage
+} from '../../utils/tools';
+import {
+  deviceBindApi
+} from '../../api/api.js'
+const app = getApp()
 var xBlufi = require("../../utils/blufi/xBlufi.js");
 var _this = null;
 var timeout = null;
@@ -6,12 +13,13 @@ var sequenceCounet = 0;
 var newScanListLength = -1;
 var oldScanListLength = 0;
 
-const SCAN_STEP = 0;
-const INPUT_STEP = 1;
-const DONE_STEP = 2;
+const SCAN_STEP = 1;
+const INPUT_STEP = 2;
+const DONE_STEP = 3;
 
 Page({
   data: {
+    phoneEnv: app.globalData.phoneEnv, // 手机系统
     blufiloadInfo: "扫描设备",
     blufiLoadStatus: false,
     blufiScanStatus: false,
@@ -21,19 +29,30 @@ Page({
     ssid: "",
     password: "",
     steps: [{
-      text: '1.连接设备'
+      text: '1.配网事项'
     },
     {
-      text: '2.填写WiFi'
+      text: '2.连接设备'
     },
     {
-      text: '3.配网成功'
+      text: '3.配置WIFI'
+    },
+    {
+      text: '4.配网成功'
     }
     ],
     deviceId: '',
     deviceName: '',
     devicesListTemp: [],
-    devicesList: []
+    devicesList: [],
+    deviceInfo: null, // 选择的设备信息
+    // 配网状态变量
+    sendSuccess: false,
+    networkSuccess: false,
+    // addSuccess: false,
+    addFlag: false,
+    deptId: '',
+    empty: false,
   },
   setValue(key, value) {
     this.setData({
@@ -55,6 +74,7 @@ Page({
           })
           console.log("start scan!")
           this.setValue("blufiloadInfo", "扫描中")
+          this.setValue('empty', false)
           this.setValue("blufiLoadStatus", true)
           interval_timer = setInterval(() => {
             this.blufiUpdateList();
@@ -68,8 +88,13 @@ Page({
           this.setValue("blufiloadInfo", "开始扫描")
           this.setValue("blufiLoadStatus", false)
         }
+        // 扫描超时
+        setTimeout(() => {
+          this.setValue('empty', true)
+        }, 30 * 1000)
         break;
       case INPUT_STEP:
+        this.setValue("stepActive", 3);
         this.startConfig()
       case DONE_STEP:
         break;
@@ -78,6 +103,10 @@ Page({
     }
   },
   blufiConnect(e) {
+    if (!this.data.deviceInfo) {
+      return showMessage('error', '请先选择要连接的设备', 2000, this);
+    }
+    let deviceId = this.data.deviceInfo.deviceId
     this.setValue("blufiloadInfo", "连接中")
     if (interval_timer) {
       clearInterval(interval_timer)
@@ -87,12 +116,12 @@ Page({
       'isStart': false
     })
     for (var i = 0; i < this.data.devicesList.length; i++) {
-      if (e.currentTarget.id === this.data.devicesList[i].deviceId) {
+      if (deviceId === this.data.devicesList[i].deviceId) {
         let name = this.data.devicesList[i].name
-        console.log('连接deviceId:' + e.currentTarget.id)
+        console.log('连接deviceId:' + deviceId)
         xBlufi.notifyConnectBle({
           isStart: true,
-          deviceId: e.currentTarget.id,
+          deviceId: deviceId,
           name
         });
         wx.showLoading({
@@ -123,9 +152,11 @@ Page({
           })
           this.blufiTimeoutClear()
           this.setValue("blufiloadInfo", "配置WiFi")
-          this.setValue("stepActive", 1);
+          this.setValue("stepActive", 2);
           this.setValue("blufiLoadStatus", false)
           this.setValue("deviceId", options.data.deviceId)
+          // 更新发送成功状态
+          this.setValue("sendSuccess", true)
           xBlufi.notifyInitBleEsp32({
             deviceId: options.data.deviceId,
           })
@@ -164,16 +195,20 @@ Page({
           if (options.data.progress == 100) {
             let ssid = options.data.ssid;
             console.log(ssid);
-            this.setValue("stepActive", 2);
             this.setValue("blufiloadInfo", "配网成功")
             this.setValue("blufiLoadStatus", false)
+            // 更新网络配置成功和添加成功状态
+            this.setValue("networkSuccess", true)
+            // if (!this.data.addFlag) {
+            //   this.addWifiDevice() // 添加设备
+            // }
             this.blufiTimeoutClear()
             timeout = setTimeout(() => {
               wx.closeBLEConnection({
                 deviceId: this.data.deviceId,
                 success: function (res) { },
               })
-              this.blufiReset()
+              this.blufiReset('ok')
             }, 1500)
           }
         }
@@ -203,7 +238,7 @@ Page({
             showCancel: false, //是否显示取消按钮
             success: function (res) {
               wx.redirectTo({
-                url: '../search/search'
+                url: '/pages/index/index'
               })
             }
           })
@@ -211,20 +246,27 @@ Page({
         break;
     }
   },
-  blufiReset: function () {
+  blufiReset: function (type) {
     wx.closeBLEConnection({
       deviceId: this.data.deviceId,
       success: function (res) { },
     })
+    if (type !== 'ok') {
+      this.setValue("stepActive", 0)
+      // 重置配网状态
+      this.setValue("sendSuccess", false)
+      this.setValue("networkSuccess", false)
+      // this.setValue("addSuccess", false)
+    }
     this.setValue("blufiloadInfo", "扫描设备")
     this.setValue("blufiLoadStatus", false)
-    this.setValue("stepActive", 0)
     this.setValue("devicesList", [])
     this.setValue("deviceId", '')
     this.setValue("macFilter", '')
   },
   blufiUpdateList: function () {
-    let list = this.data.devicesListTemp
+    console.log('data.devicesListTemp', this.data.devicesListTemp)
+    let list = this.data.devicesListTemp.filter(item => item.localName && item.localName.indexOf('GFKM-') === 0);
     var listLen = list.length;
     for (var i = 0; i < listLen - 1; i++) {
       for (var j = 0; j < listLen - 1 - i; j++) {
@@ -235,17 +277,13 @@ Page({
         }
       }
     }
-    // console.log(list)
     _this.setData({
       devicesList: list
     });
   },
   blufiTimeoutHandle: function () {
     wx.hideLoading()
-    wx.showToast({
-      title: '配网超时, 复位设备重试',
-      icon: 'none'
-    })
+    this.message('error', '配网超时，请重试！', 3000)
     this.blufiReset()
     timeout = null;
   },
@@ -259,21 +297,22 @@ Page({
       clearInterval(interval_timer)
     }
   },
-  encodeUtf8: function(text) {
+  encodeUtf8: function (text) {
     const code = encodeURIComponent(text);
     const bytes = [];
     for (var i = 0; i < code.length; i++) {
-        const c = code.charAt(i);
-        if (c === '%') {
-            const hex = code.charAt(i + 1) + code.charAt(i + 2);
-            const hexVal = parseInt(hex, 16);
-            bytes.push(hexVal);
-            i += 2;
-        } else bytes.push(c.charCodeAt(0));
+      const c = code.charAt(i);
+      if (c === '%') {
+        const hex = code.charAt(i + 1) + code.charAt(i + 2);
+        const hexVal = parseInt(hex, 16);
+        bytes.push(hexVal);
+        i += 2;
+      } else bytes.push(c.charCodeAt(0));
     }
     return bytes;
   },
-  onLoad: function () {
+  onLoad: function (options) {
+    this.setValue("deptId", options.deptId || "")
     _this = this;
     xBlufi.initXBlufi(1);
     console.log("xBlufi", xBlufi.XMQTT_SYSTEM)
@@ -294,7 +333,12 @@ Page({
       deviceId: _this.data.deviceId,
       name: _this.data.deviceName,
     });
-    xBlufi.listenDeviceMsgEvent(false, this.funListenDeviceMsgEvent);
+    xBlufi.notifyStartDiscoverBle({
+      'isStart': true
+    })
+    xBlufi.listenDeviceMsgEvent(true, this.blufiEventHandler);
+    this.blufiIntercalClear()
+    this.blufiTimeoutClear()
   },
   onShow: function (options) { },
   filterChange(event) {
@@ -312,11 +356,7 @@ Page({
     wx.getConnectedWifi({
       success: function (res) {
         if (res.wifi.SSID.indexOf("5G") != -1) {
-          wx.showToast({
-            title: '不支持配置5G WiFi网络',
-            icon: 'none',
-            duration: 3000
-          })
+          this.message('error', '不支持配置5G WiFi网络', 3000)
         }
         let password = wx.getStorageSync(res.wifi.SSID)
         console.log("restore password:", password)
@@ -345,25 +385,21 @@ Page({
   },
   _startConfig: function () {
     if (!this.data.ssid) {
-      wx.showToast({
-        title: 'SSID不能为空',
-        icon: 'none'
-      });
+      this.message('error', 'WIFI名称不能为空', 3000)
       return;
     }
     if (!this.data.password) {
-      wx.showToast({
-        title: '密码不能为空',
-        icon: 'none'
-      });
+      this.message('error', 'WIFI密码不能为空', 3000)
       return;
     }
-    wx.showLoading({
-      title: '正在配网',
-      mask: true
-    });
+    // wx.showLoading({
+    //   title: '正在配网',
+    //   mask: true
+    // });
     this.setValue("blufiloadInfo", "配网中")
     this.setValue("blufiLoadStatus", true)
+    // 开始WiFi配置，设置网络配置状态为进行中
+    this.setValue("networkSuccess", false)
     // xBlufi.notifySendRouterSsidAndPassword({
     //   ssid: this.data.ssid,
     //   password: this.data.password
@@ -374,7 +410,7 @@ Page({
     let connect_payload = [0x0C, 0x00, 0x02, sequenceCounet++];
 
     var temp_ssid_payload = []
-    for(var i = 0; i < this.data.ssid.length; i++){
+    for (var i = 0; i < this.data.ssid.length; i++) {
       var ssid_utf8 = this.encodeUtf8(this.data.ssid[i])
       console.log(ssid_utf8)
       temp_ssid_payload.push(...ssid_utf8);
@@ -383,7 +419,7 @@ Page({
     ssid_payload.push(temp_ssid_payload.length);
     ssid_payload.push(...temp_ssid_payload);
     var temp_pwd_payload = []
-    for(var i = 0; i < this.data.password.length; i++){
+    for (var i = 0; i < this.data.password.length; i++) {
       var pwd_utf8 = this.encodeUtf8(this.data.password[i])
       console.log(pwd_utf8)
       temp_pwd_payload.push(...pwd_utf8);
@@ -412,5 +448,63 @@ Page({
   },
   onShareAppMessage: function () {
     /// ignore
+  },
+  // 下一步
+  nextBtnHandle() {
+    this.setData({
+      stepActive: this.data.stepActive + 1
+    })
+
+    // 扫描设备
+    this.blufiBtnHandle()
+  },
+  // 选择设备
+  bindChange(e) {
+    let {
+      value
+    } = e?.detail
+    this.setData({
+      deviceInfo: value
+    })
+  },
+  // // 添加设备到团队
+  // addWifiDevice() {
+  //   let { deptId, deviceInfo } = this.data
+  //   // 去掉deviceInfo.localName前面的GFKM-
+  //   deviceBindApi(deptId, deviceInfo.localName).then(res => {
+  //     if (res.code === 200) {
+  //       this.setValue("addFlag", true);
+  //       this.message('success', '设备绑定成功！', 2000)
+  //       setTimeout(() => {
+  //         this.setValue("addSuccess", true)
+  //       }, 1500);
+  //     } else {
+  //       console.log("绑定失败", res)
+  //       this.message('error', res.msg, 3000)
+  //       setTimeout(() => {
+  //         this.goBack()
+  //       }, 2000);
+  //     }
+  //   })
+  // },
+  startWifiInfo() {
+    // 判断wifi名称和密码不能为空
+    if (!this.data.ssid) {
+      this.message('error', 'WIFI名称不能为空', 3000)
+      return;
+    }
+    if (!this.data.password) {
+      this.message('error', 'WIFI密码不能为空', 3000)
+      return;
+    }
+    this.blufiBtnHandle();
+  },
+  goBack() {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
+  message(type, text, duration = 1500) {
+    showMessage(type, text, duration, this);
   }
 });
