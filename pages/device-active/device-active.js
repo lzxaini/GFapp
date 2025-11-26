05141346dc463c2f3ba1565ba8666f83d1fab271
17630921248 <1245634367@qq.com>
2025-11-26 10:36:21 +0800

    feature: 新增激活后移机退网操作和引导

--- pages/device-active/device-active.js
+++ pages/device-active/device-active.js
@@ -2,7 +2,9 @@ import {
   onMqttReady
 } from '../../utils/mqttReady';
 const app = getApp();
-import { showMessage } from '../../utils/tools';
+import {
+  showMessage
+} from '../../utils/tools';
 Page({
   data: {
     marginBottom: app.globalData.marginBottom,
@@ -12,6 +14,14 @@ Page({
     reconnectMaxCount: 20, // 最多重试次数
     reconnectCount: 0, // 当前重试次数
     reconnectTimer: null, // 定时器引用
+    resetNetwork: false, // 是否重新配网
+    showGuide: false, // 是否显示引导
+    current: 0, // 当前步骤
+    steps: [],
+    btnObj: {
+      content: '取消',
+      theme: 'danger'
+    }
   },
   onLoad(option) {
     const {
@@ -71,6 +81,73 @@ Page({
         url: `/pages/device-use/device-use?deviceId=${this.data.deviceId}`,
       });
     }
+    if (topic === `/resp/${this.data.deviceId}` && result?.string === this.data.deviceId && this.data.resetNetwork) {
+      wx.showLoading({
+        title: '重置中...',
+      })
+      const mqttQrotocol = app.globalData.mqttQrotocol;
+      mqttQrotocol.sendString('network-reset', `/req/${this.data.deviceId}`);
+      setTimeout(() => {
+        mqttQrotocol.sendString('network-reset', `/req/${this.data.deviceId}`);
+        setTimeout(() => {
+          wx.hideLoading()
+          wx.navigateBack(1);
+        }, 1000);
+      }, 7 * 1000)
+    }
+  },
+
+  // 引导
+  attached() {
+    this.setData({
+      current: 0,
+      showGuide: true,
+      steps: [{
+        element: () =>
+          new Promise((resolve) =>
+            this.createSelectorQuery()
+            .select('.item1')
+            .boundingClientRect((rect) => resolve(rect))
+            .exec(),
+          ),
+        title: '重要提醒',
+        body: '您将对该设备，进行重置网络的操作，重置网络后可对该设备重新联网，重置网络的操作不可逆，请您确定！',
+        placement: 'bottom',
+      }, {
+        element: () =>
+          new Promise((resolve) =>
+            this.createSelectorQuery()
+            .select('.item2')
+            .boundingClientRect((rect) => resolve(rect))
+            .exec(),
+          ),
+        title: '重置网络',
+        body: '确定设备为通电状态，并且保持设备的网络在线！',
+        placement: 'center',
+      }, {
+        element: () =>
+          new Promise((resolve) =>
+            this.createSelectorQuery()
+            .select('.item3')
+            .boundingClientRect((rect) => resolve(rect))
+            .exec(),
+          ),
+        title: '重置网络',
+        body: '设备重置网络的过程中，请勿将设备断电，等待页面提醒！',
+        placement: 'center',
+      }, {
+        element: () =>
+          new Promise((resolve) =>
+            this.createSelectorQuery()
+            .select('.item4')
+            .boundingClientRect((rect) => resolve(rect))
+            .exec(),
+          ),
+        title: '重置网络',
+        body: '不可撤销，点击重置按钮，设备将重新进入网络配置模式！',
+        placement: 'center',
+      }],
+    });
   },
   // === 启动扫码连接 + 定时重连逻辑 ===
   connectDevice() {
@@ -114,5 +191,38 @@ Page({
         reconnectCount: 0
       });
     }
-  }
+  },
+  // === 显示设备移机引导 ===
+  showDeviceMoveGuide() {
+    this.attached();
+  },
+  // === 重置设备配网 ===
+  resetDeviceNetwork() {
+    console.log('adasd', this.data.deviceId)
+    const mqttQrotocol = app.globalData.mqttQrotocol;
+    mqttQrotocol.sendString('get-qr-code', `/req/${this.data.deviceId}`);
+    this.setData({
+      resetNetwork: true
+    })
+  },
+  // === 引导步骤变化 ===
+  handleGuideChange(e) {
+    const {
+      current
+    } = e.detail;
+    this.setData({
+      current
+    });
+  },
+  // === 跳过引导 ===
+  handleGuideSkip() {
+    this.setData({
+      current: 0,
+      showGuide: false,
+    });
+  },
+  // === 完成引导并重置设备 ===
+  handleGuideFinish() {
+    console.log('完成引导')
+  },
 });
