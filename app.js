ed576872d4fdb484e88d6f516d803ab9950f16cc
17630921248 <1245634367@qq.com>
2025-12-16 14:11:24 +0800

    feature: 体验版0.0.21_25121613。修复自定义返回导致的页面返回出现白屏。优化开发模式返回逻辑

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ App({
     appName: {
       name: '光馥科美',
       slogan: '光塑年轻力，馥养无龄美',
-      appVersion: '0.0.20_25121513',
+      appVersion: '0.0.21_25121613',
     },
     phoneEnv: '', // 手机系统
     mqttClient: null,
