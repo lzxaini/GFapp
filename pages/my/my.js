c9b70a0f6e3645b55aed8235823ae6ec70921aa0
17630921248 <1245634367@qq.com>
2025-12-26 18:58:18 +0800

    feature: 我的页面完善

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,4 +1,10 @@
 import tabService from '../../utils/tab-service';
+import {
+  showMessage
+} from '../../utils/tools';
+import {
+  withLogin
+} from '../../utils/auth';
 const app = getApp()
 Page({
 
@@ -7,6 +13,11 @@ Page({
    */
   data: {
     userInfo: getApp().globalData.userInfo,
+    capsuleHeight: app.globalData.capsuleHeight,
+    marginBottom: app.globalData.marginBottom,
+    appName: app.globalData.appName,
+    ossUrl: app.globalData.ossUrl,
+    isLogin: false
   },
 
   /**
@@ -17,10 +28,19 @@ Page({
     if (userInfo) {
       tabService.updateRole(this, userInfo.dept.deptType)
       console.log('options', tabService, userInfo)
+    } else{
+      tabService.updateRole(this, '4')
     }
     wx.switchTab({
       url: '/pages/my/my'
     })
+
+    this.goRechargeHistory = withLogin(this, this._goRechargeHistory);
+    this.goSystem = withLogin(this, this._goSystem);
+    this.goServiceHistory = withLogin(this, this._goServiceHistory);
+    this.addDept = withLogin(this, this._addDept);
+    this.goMyDept = withLogin(this, this._goMyDept);
+    this.goLoginFlag = withLogin(this, this._goLoginFlag);
   },
 
   /**
@@ -71,5 +91,77 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+  message(type, text, duration = 1500) {
+    showMessage(type, text, duration, this);
+  },
+  _goLoginFlag() {
+    console.log('点击名字区域')
+  },
+  _goRechargeHistory() {
+    this.navigateByTitle({
+      title: '支付记录',
+      url: '/pages/recharge-history/recharge-history'
+    });
+  },
+  _goServiceHistory() {
+    this.navigateByTitle({
+      title: '服务记录',
+      url: '/pages/service-history/service-history'
+    });
+  },
+  _goSystem() {
+    this.navigateByTitle({
+      title: '白名单',
+      url: '/pages/my-edit/my-edit'
+    });
+  },
+  _addDept(){
+    this.navigateByTitle({
+      title: '新建团队',
+      url: '/pages/add-dept/add-dept'
+    });
+  },
+  _goMyDept(){
+    this.navigateByTitle({
+      title: '我的团队',
+      url: '/pages/team-list/team-list'
+    });
+  },
+  goLogin() {
+    wx.reLaunch({
+      url: '/pages/login/login',
+    });
+  },
+  // 公共跳转方法
+  navigateByTitle({
+    title,
+    url
+  }) {
+    if (title === '设置') {
+      return wx.navigateTo({
+        url
+      });
+    }
+
+    if (this.verifyDept()) return;
+    wx.navigateTo({
+      url
+    });
+  },
+  verifyDept() {
+    const {
+      dept
+    } = this.data.userInfo || {};
+    if (!dept) {
+      this.message('warning', '游客账号暂时无法使用，请联系管理员！');
+      return true;
+    }
+    return false;
+  },
+  closeIsLoginDialog() {
+    this.setData({
+      isLogin: false
+    });
+  },
 })
