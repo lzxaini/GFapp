26c426fadb7049b0c17996282cc1916940ee1ba2
17630921248 <1245634367@qq.com>
2026-01-06 16:49:42 +0800

    feature: 第一个0.0.1_260106164发布

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ App({
     appName: {
       name: '光馥科美',
       slogan: '光塑年轻力，馥养无龄美',
-      appVersion: '0.0.1_26010616',
+      appVersion: '0.0.1_260106164',
     },
     phoneEnv: '', // 手机系统
     mqttClient: null,
