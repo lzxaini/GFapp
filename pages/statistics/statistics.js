82219bb4be61525ff82fc820dc2a08731cc74990
17630921248 <1245634367@qq.com>
2026-04-15 13:12:32 +0800

    feature: 新增接口对接

--- pages/statistics/statistics.js
+++ pages/statistics/statistics.js
@@ -1,5 +1,9 @@
 import tabService from '../../utils/tab-service';
 import {
+  getDashboardApi,
+  getUserExtendListApi
+} from '../../api/api'
+import {
   showMessage
 } from '../../utils/tools';
 const dayjs = require('dayjs')
@@ -9,11 +13,26 @@ Page({
     capsuleHeight: app.globalData.capsuleHeight,
     userInfo: getApp().globalData.userInfo,
     ossUrl: app.globalData.ossUrl,
-    searchFlag: false
+    dashboard: {
+      activeDeviceCount: 0,
+      remainingPoints: 0,
+      todayConsumedPoints: 0,
+      todayRechargeAmount: 0,
+      todayConsumedPointsGrowthRate: 0,
+      todayRechargeAmountGrowthRate: 0
+    },
+    tableData: [],
+    total: 0,
+    refresher: false,
+    pageObj: {
+      pageNum: 1,
+      pageSize: 10,
+      deptName: ""
+    },
+    searchFlag: false,
+    searchName: ""
   },
-  onLoad() {
-    this.getRegion()
-  },
+  onLoad() {},
   onShow() {
     //更新底部高亮
     tabService.updateIndex(this, 1)
@@ -22,20 +41,88 @@ Page({
     this.setData({
       userInfo,
     })
+    this.getDashboard()
+    this.getUserExtendList()
   },
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
+  onReachBottom(e) {
+    // let direction = e?.detail?.direction
+    // if (direction === 'right') {
+    //   return;
+    // }
+    let {
+      tableData,
+      total
+    } = this.data
+    if (tableData.length < total) {
+      let pageNum = ++this.data.pageObj.pageNum
+      this.setData({
+        'pageObj.pageNum': pageNum
+      })
+      this.getUserExtendList('bottom')
+    }
   },
   pullDownToRefresh() {
+    this.setData({
+      'pageObj.pageNum': 1
+    })
+    this.getUserExtendList()
+    this.getDashboard()
   },
-  searchClick(){
+  searchClick() {
     this.setData({
       searchFlag: true
     })
   },
-  blurClick(){
+  // 获取信息
+  getDashboard() {
+    getDashboardApi().then(res => {
+      if (res.code === 200) {
+        this.setData({
+          dashboard: res.data
+        })
+      }
+    })
+  },
+  // 搜索
+  changeHandle(e) {
+    let {
+      value
+    } = e?.detail
+    this.setData({
+      'pageObj.deptName': value
+    })
+    this.getUserExtendList()
+  },
+  /**
+   * 分页查询
+   * 增加在分页条件里面
+   * @param {*} type 
+   */
+  getUserExtendList(type = 'init') {
+    getUserExtendListApi(this.data.pageObj).then(res => {
+      if (type === 'bottom') {
+        if (res.data.rows.length > 0) {
+          let list = this.data.tableData
+          list.push(...res.data.rows)
+          this.setData({
+            tableData: list
+          })
+        }
+      } else {
+        this.setData({
+          tableData: res.data.rows,
+          total: res.data.total
+        })
+      }
+      this.setData({
+        refresher: false
+      })
+    })
+  },
+  blurClick() {
     this.setData({
       searchFlag: false
     })
