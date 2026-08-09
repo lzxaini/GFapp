31dafabe1cec5699e60bec88496c4dca32d9105b
lzx <1245634367@qq.com>
2026-08-09 22:13:48 +0800

    feat: 新增服务项目直接查看隐藏展开入口

--- other/use-history/use-history.js
+++ other/use-history/use-history.js
@@ -1,5 +1,11 @@
-import { getServiceSessionListApi, getSessionDetailApi } from '../../api/api'
-import { getServiceNameByCode, getDeviceStatusIconByCode } from '../../utils/config'
+import {
+  getServiceSessionListApi,
+  getSessionDetailApi
+} from '../../api/api'
+import {
+  getServiceNameByCode,
+  getDeviceStatusIconByCode
+} from '../../utils/config'
 const app = getApp()
 Page({
   data: {
@@ -19,17 +25,22 @@ Page({
     }
   },
   onLoad(options) {
-    let { serialNumber } = options
+    let {
+      serialNumber
+    } = options
     this.setData({
       'pageObj.serialNumber': serialNumber
     })
     this.getDeviceUseList()
   },
   /**
-  * 页面上拉触底事件的处理函数
- */
+   * 页面上拉触底事件的处理函数
+   */
   onReachBottom() {
-    let { historyList, total } = this.data
+    let {
+      historyList,
+      total
+    } = this.data
     if (historyList.length < total) {
       let pageNum = ++this.data.pageObj.pageNum
       this.setData({
@@ -52,7 +63,7 @@ Page({
     const sessionId = e.currentTarget.dataset.sessionid
     const index = e.currentTarget.dataset.index
     const item = this.data.historyList[index]
-    
+
     // 如果已经展开，则折叠
     if (item.expanded) {
       this.setData({
@@ -61,7 +72,7 @@ Page({
       })
       return
     }
-    
+
     // 加载详情数据
     getSessionDetailApi(sessionId).then(res => {
       const details = res.data.map(detail => ({
@@ -75,7 +86,27 @@ Page({
       })
     })
   },
-
+  /**
+   * 查询项目详情，返回项目名称字符串（如 "项目1/项目2/项目3"）
+   * @param {*} sessionId 会话ID
+   * @returns {Promise<string>} 项目名称拼接的字符串
+   */
+  getProjectDetail(e) {
+    const sessionId = e.currentTarget.dataset.sessionid
+    const index = e.currentTarget.dataset.index
+    // 加载项目，项目1/项目2/项目3
+    getSessionDetailApi(sessionId).then(res => {
+      // 遍历所有子详情，提取项目名称
+      const projectNames = res.data.map(detail => {
+        const serviceObj = getServiceNameByCode(detail.service)
+        return serviceObj ? serviceObj.name : ''
+      }).filter(name => name) // 过滤掉未匹配到的空名称
+      // return projectNames.join('/')
+      this.setData({
+        [`historyList[${index}].projectNames`]: projectNames.join('/')
+      })
+    })
+  },
   /**
    * 查询设备使用记录（使用会话聚合接口，同一50分钟周期合并为一条）
    * @param {*} type 
@@ -86,8 +117,8 @@ Page({
         ...item,
         serviceObj: getServiceNameByCode(item.service),
         statusIcon: getDeviceStatusIconByCode(item.status),
-        expanded: false,  // 是否展开详情
-        details: []       // 详细记录列表
+        expanded: false, // 是否展开详情
+        details: [] // 详细记录列表
       }))
       if (type === 'bottom') {
         if (rows.length > 0) {
