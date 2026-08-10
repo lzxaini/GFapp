9173534db94a396caab1d68d88c7e8e65f2ddb2c
17630921248 <1245634367@qq.com>
2026-08-10 11:44:46 +0800

    feat: 发布1.0.6_260810114版本

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ App({
     appName: {
       name: '玑肤芮',
       slogan: '年轻计划，轻松开肌',
-      appVersion: '1.0.5_260808215',
+      appVersion: '1.0.6_260810114',
       beian: '粤ICP备2026009218号-1X',
     },
     phoneEnv: '', // 手机系统
