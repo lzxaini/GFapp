47580edddfbdd52959dbacef072c474b193dba84
17630921248 <1245634367@qq.com>
2026-08-11 16:00:15 +0800

    feat: 新增支持直接查看服务项目注释展开详情

--- other/service-history/service-history.js
+++ other/service-history/service-history.js
@@ -1,5 +1,11 @@
-import { getServiceSessionListApi, getSessionDetailApi } from '../../api/api'
-import { getServiceNameByCode, getDeviceStatusIconByCode } from '../../utils/config'
+import {
+  getServiceSessionListApi,
+  getSessionDetailApi
+} from '../../api/api'
+import {
+  getServiceNameByCode,
+  getDeviceStatusIconByCode
+} from '../../utils/config'
 const app = getApp()
 Page({
   data: {
@@ -17,7 +23,9 @@ Page({
     }
   },
   onShow() {
-    let { dept } = app.globalData.userInfo
+    let {
+      dept
+    } = app.globalData.userInfo
     if (dept.deptType != 1) {
       this.setData({
         'pageObj.deptId': dept.deptId
@@ -26,11 +34,14 @@ Page({
     this.getServiceRecords()
   },
   /**
- * 页面上拉触底事件的处理函数
- */
+   * 页面上拉触底事件的处理函数
+   */
   onReachBottom() {
-    console.log('触底',)
-    let { tableData, total } = this.data
+    console.log('触底', )
+    let {
+      tableData,
+      total
+    } = this.data
     if (tableData.length < total) {
       let pageNum = ++this.data.pageObj.pageNum
       this.setData({
@@ -59,7 +70,7 @@ Page({
     const sessionId = e.currentTarget.dataset.sessionid
     const index = e.currentTarget.dataset.index
     const item = this.data.tableData[index]
-    
+
     // 如果已经展开，则折叠
     if (item.expanded) {
       this.setData({
@@ -68,7 +79,7 @@ Page({
       })
       return
     }
-    
+
     // 加载详情数据
     getSessionDetailApi(sessionId).then(res => {
       const details = res.data.map(detail => ({
@@ -83,8 +94,29 @@ Page({
     })
   },
   /**
-    * 分页查询根据不同时间节点：range:1今天 2昨天 3本周 4本月
-    * 使用新的会话聚合接口，同一50分钟周期的记录会合并为一条
+   * 查询项目详情，返回项目名称字符串（如 "项目1/项目2/项目3"）
+   * @param {*} sessionId 会话ID
+   * @returns {Promise<string>} 项目名称拼接的字符串
+   */
+  getProjectDetail(e) {
+    const sessionId = e.currentTarget.dataset.sessionid
+    const index = e.currentTarget.dataset.index
+    // 加载项目，项目1/项目2/项目3
+    getSessionDetailApi(sessionId).then(res => {
+      // 遍历所有子详情，提取项目名称
+      const projectNames = res.data.map(detail => {
+        const serviceObj = getServiceNameByCode(detail.service)
+        return serviceObj ? serviceObj.name : ''
+      }).filter(name => name) // 过滤掉未匹配到的空名称
+      // return projectNames.join('/')
+      this.setData({
+        [`tableData[${index}].projectNames`]: projectNames.join('/')
+      })
+    })
+  },
+  /**
+   * 分页查询根据不同时间节点：range:1今天 2昨天 3本周 4本月
+   * 使用新的会话聚合接口，同一50分钟周期的记录会合并为一条
    * @param {*} type 
    */
   getServiceRecords(type = 'init') {
@@ -94,8 +126,8 @@ Page({
         ...item,
         serviceObj: getServiceNameByCode(item.service),
         statusIcon: getDeviceStatusIconByCode(item.status),
-        expanded: false,  // 是否展开详情
-        details: []       // 详细记录列表
+        expanded: false, // 是否展开详情
+        details: [] // 详细记录列表
       }))
       if (type === 'bottom') {
         if (rows.length > 0) {
--- pages/operation/operation.js
+++ pages/operation/operation.js
@@ -218,6 +218,27 @@ Page({
       }
     })
   },
+  /**
+   * 查询项目详情，返回项目名称字符串（如 "项目1/项目2/项目3"）
+   * @param {*} sessionId 会话ID
+   * @returns {Promise<string>} 项目名称拼接的字符串
+   */
+  getProjectDetail(e) {
+    const sessionId = e.currentTarget.dataset.sessionid
+    const index = e.currentTarget.dataset.index
+    // 加载项目，项目1/项目2/项目3
+    getSessionDetailApi(sessionId).then(res => {
+      // 遍历所有子详情，提取项目名称
+      const projectNames = res.data.map(detail => {
+        const serviceObj = getServiceNameByCode(detail.service)
+        return serviceObj ? serviceObj.name : ''
+      }).filter(name => name) // 过滤掉未匹配到的空名称
+      // return projectNames.join('/')
+      this.setData({
+        [`serviceList[${index}].projectNames`]: projectNames.join('/')
+      })
+    })
+  },
   searchInput(e) {
     let {
       value
