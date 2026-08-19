509883cbd609698712da564b80d9e9c9bd650b9b
17630921248 <1245634367@qq.com>
2026-08-19 15:19:35 +0800

    feat: 发布0.0.4_260819151版本

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ App({
     appName: {
       name: '超素美',
       slogan: '爱上素颜美，就选超素美',
-      appVersion: '0.0.3_260811160',
+      appVersion: '0.0.4_260819151',
       beian: '粤ICP备2026100471号-2X',
       copyright: 'Copyright © 2020-2026 CSM.All Rights Reserved.',
     },
