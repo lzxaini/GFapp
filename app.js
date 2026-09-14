ce553975546d13d407bfa959cf523af1ff426c31
17630921248 <1245634367@qq.com>
2026-09-14 13:25:33 +0800

    feat: 发布0.0.5_260914132，优化部分页面，修复部分已知问题

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ App({
       weChatAppName: 'HEYITTE逆龄中心',
       name: '禾壹净',
       slogan: '爱上美好，就选禾壹净',
-      appVersion: '0.0.5_260912213',
+      appVersion: '0.0.5_260914132',
       beian: '皖ICP备2026021999号-2X',
       copyright: 'Copyright © 2020-2026 HYJ.All Rights Reserved.',
     },
